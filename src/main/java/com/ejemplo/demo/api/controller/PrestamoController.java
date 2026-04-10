59564de4f25d81cd225d2a92b05c97615d9ad400
Lester David Payes Méndez <lpayesm@miumg.edu.gt>
2026-04-09 21:32:51 -0600

    relice paso numero 8, agregue todo lo necesario tanto codigo como captura, respetando el codigo limpio

---
+++ src/main/java/com/ejemplo/demo/api/controller/PrestamoController.java
@@ -0,0 +1,27 @@
+package com.ejemplo.demo.api.controller;
+
+import com.ejemplo.demo.api.dto.PrestamoRequest;
+import com.ejemplo.demo.api.dto.PrestamoResponse;
+import com.ejemplo.demo.domain.service.PrestamoService; // El Inge lo puso en .domain.service
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/simulaciones")
+@Tag(name = "Simulador de Préstamos")
+public class PrestamoController {
+
+    private final PrestamoService prestamoService;
+
+    public PrestamoController(PrestamoService prestamoService) {
+        this.prestamoService = prestamoService;
+    }
+
+    @PostMapping("/prestamo")
+    @Operation(summary = "Calcula la simulación")
+    public PrestamoResponse simular(@Valid @RequestBody PrestamoRequest request) {
+        return prestamoService.calcular(request);
+    }
+}
--- src/main/java/com/ejemplo/demo/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ejemplo/demo/api/exception/GlobalExceptionHandler.java
@@ -56,4 +56,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
     
+    
+    
 }
