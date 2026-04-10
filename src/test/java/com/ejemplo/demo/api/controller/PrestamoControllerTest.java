581703cee2974740460b93f590f4ef4924f80ced
Lester David Payes Méndez <lpayesm@miumg.edu.gt>
2026-04-09 22:40:55 -0600

    ultimo cambio

--- src/test/java/com/ejemplo/demo/api/controller/PrestamoControllerTest.java
+++ src/test/java/com/ejemplo/demo/api/controller/PrestamoControllerTest.java
@@ -2,7 +2,7 @@ package com.ejemplo.demo.api.controller;
 
 import com.ejemplo.demo.api.dto.PrestamoRequest;
 import com.ejemplo.demo.api.dto.PrestamoResponse;
-import com.ejemplo.demo.domain.service.PrestamoService; // Importante para que lo encuentre
+import com.ejemplo.demo.domain.service.PrestamoService; 
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
